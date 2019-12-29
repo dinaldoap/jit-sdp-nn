06dc6f8024e43c8f02e4575a7302b53b7192f93e
dinaldoap <dinaldoap@gmail.com>
2019-12-29 23:19:55 +0000

    refactor: move evaluation functions

---
+++ jitsdp/constants.py
@@ -0,0 +1,1 @@
+N_CLASSES = 2
---
+++ jitsdp/metrics.py
@@ -0,0 +1,38 @@
+import numpy as np
+import torch
+from scipy.stats import mstats
+import jitsdp.constants as const
+
+
+def loss(classifier, dataloader, criterion):
+  with torch.no_grad():
+    classifier.eval()
+    loss = 0
+    for inputs, targets in dataloader:
+      if torch.cuda.is_available():
+        inputs, targets = inputs.cuda(), targets.cuda()
+
+      outputs = classifier(inputs.float())
+      batch_loss = criterion(outputs.squeeze(), targets.float())
+      loss += batch_loss.item()
+
+    return loss / len(dataloader)
+
+def __recalls(targets, predictions):
+  confusion_matrix, _, _ = np.histogram2d(targets.detach().cpu().numpy(), predictions.squeeze().detach().cpu().numpy(), bins=const.N_CLASSES)
+  recalls = np.diag(confusion_matrix) / ((np.sum(confusion_matrix, axis=1)) + 1e-12)
+  return recalls
+
+def gmean(classifier, dataloader):
+  with torch.no_grad():
+    classifier.eval()
+    recalls = np.zeros((const.N_CLASSES))    
+    for inputs, targets in dataloader:
+      if torch.cuda.is_available():
+        inputs, targets = inputs.cuda(), targets.cuda()
+      
+      outputs = classifier(inputs.float())
+      predictions = torch.round(outputs).int()
+      recalls += __recalls(targets, predictions)
+    recalls = recalls / len(dataloader)
+    return mstats.gmean(recalls), recalls
