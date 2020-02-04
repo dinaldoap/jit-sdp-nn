8c722f1340dc077cf078def2f0e4828a37ae98c9
dinaldoap <dinaldoap@gmail.com>
2020-02-04 23:35:38 +0000

    test: return DataFrame

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,5 +1,7 @@
 from jitsdp import metrics
 import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
 
 from pytest import approx
 
@@ -8,7 +10,10 @@ def test_prequential_recalls():
     fading_factor = 1
     targets = [0, 1, 0, 0, 1, 1]
     predictions = [1, 0, 0, 0, 1, 1]
-    expected = np.array(
-        [[0, 0], [0, 0], [.5, 0], [.6666666, 0], [.6666666, .5], [.6666666, .6666666]])
+    expected = {
+        'r0': [0, 0, .5, .6666666, .6666666, .6666666],
+        'r1': [0, 0,  0,        0,       .5, .6666666]        
+    }
+    expected = pd.DataFrame(expected)
     actual = metrics.prequential_recalls(targets, predictions, fading_factor)
-    assert expected == approx(actual)
+    assert_frame_equal(expected, actual)
