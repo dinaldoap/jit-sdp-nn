b7f38e0a203906f119bbe4ba01967d2676bfbea2
dinaldoap <dinaldoap@gmail.com>
2020-02-05 12:37:32 +0000

    refactor: format

--- jitsdp/metrics.py
+++ jitsdp/metrics.py
@@ -27,18 +27,23 @@ def __recalls(targets, predictions):
     recalls = np.zeros(const.N_CLASSES)
     if n_classes == 0:
         return recalls
-    confusion_matrix, _, _ = np.histogram2d(targets, predictions, bins=[n_classes, const.N_CLASSES])
-    recalls[classes] = np.diag(confusion_matrix) / np.sum(confusion_matrix, axis=1)
+    confusion_matrix, _, _ = np.histogram2d(
+        targets, predictions, bins=[n_classes, const.N_CLASSES])
+    recalls[classes] = np.diag(confusion_matrix) / \
+        np.sum(confusion_matrix, axis=1)
     return recalls
+
 
 def __gmean(recalls):
     if np.isin(0.0, recalls):
         return 0.0
     return mstats.gmean(recalls)
 
+
 def gmean_recalls(targets, predictions):
     recalls = __recalls(targets, predictions)
     return __gmean(recalls), recalls
+
 
 def classifier_gmean_recalls(classifier, dataloader):
     if torch.cuda.is_available():
@@ -54,7 +59,8 @@ def classifier_gmean_recalls(classifier, dataloader):
             outputs = classifier(inputs.float())
             predictions = torch.round(outputs).int()
             predictions = predictions.view(predictions.shape[0])
-            recalls += __recalls(targets.detach().cpu().numpy(), predictions.detach().cpu().numpy())
+            recalls += __recalls(targets.detach().cpu().numpy(),
+                                 predictions.detach().cpu().numpy())
         recalls = recalls / len(dataloader)
         return __gmean(recalls), recalls
 
@@ -88,10 +94,12 @@ def prequential_recalls(results, fading_factor):
     recalls = pd.DataFrame(recalls, columns=columns)
     return pd.concat([results, recalls], axis='columns')
 
+
 def prequential_gmean(recalls):
     gmean = mstats.gmean(recalls[['r0', 'r1']], axis=1)
     gmean = pd.DataFrame(gmean, columns=['gmean'])
     return pd.concat([recalls, gmean], axis='columns')
+
 
 def prequential_recalls_gmean(results, fading_factor):
     return prequential_gmean(prequential_recalls(results, fading_factor))
