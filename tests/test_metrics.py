706f3df1b5570491d5717791257f0e34077024cc
dinaldoap <dinaldoap@gmail.com>
2020-02-05 12:06:51 +0000

    test: receive DataFrame as argument

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -8,13 +8,17 @@ from pytest import approx
 
 def test_prequential_recalls():
     fading_factor = 1
-    targets = [0, 1, 0, 0, 1, 1]
-    predictions = [1, 0, 0, 0, 1, 1]
-    expected = {
+    results = {
         'timestep': [0, 1, 2, 3, 4, 5],
-        'r0': [0, 0, .5, .6666666, .6666666, .6666666],
-        'r1': [0, 0,  0,        0,       .5, .6666666]        
+        'target': [0, 1, 0, 0, 1, 1],
+        'prediction': [1, 0, 0, 0, 1, 1],
     }
+    expected = results.copy()
+    expected.update({
+        'r0': [0, 0, .5, .6666666, .6666666, .6666666],
+        'r1': [0, 0,  0,        0,       .5, .6666666],    
+    })
+    results = pd.DataFrame(results)
     expected = pd.DataFrame(expected)
-    actual = metrics.prequential_recalls(targets, predictions, fading_factor)
+    actual = metrics.prequential_recalls(results, fading_factor)
     assert_frame_equal(expected, actual)
