82fb7d1f142731739f4b5f5aef264998bc694b62
dinaldoap <dinaldoap@gmail.com>
2020-02-06 13:39:09 +0000

    refactor: encapsulate preprocessing in a module

---
+++ jitsdp/data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import re
+
+
+FEATURES = ['fix', 'ns', 'nd', 'nf', 'entrophy', 'la',
+            'ld', 'lt', 'ndev', 'age', 'nuc', 'exp', 'rexp', 'sexp']
+LABEL = 'contains_bug'
+
+
+def make_stream(url):
+    df_raw = download(url)
+    df_preprocess = preprocess(df_raw)
+    return prequential(df_preprocess)
+
+
+def download(url):
+    return pd.read_csv(url)
+
+
+def preprocess(df_raw):
+    preprocess_cols = ['commit_hash', 'author_date_unix_timestamp',
+                       'fixes'] + FEATURES + [LABEL]
+    df_preprocess = df_raw[preprocess_cols].copy()
+    # timestamp
+    df_preprocess = df_preprocess.rename(columns={'author_date_unix_timestamp': 'timestamp',
+                                                  LABEL: 'target'})
+    # filter rows with missing data
+    df_preprocess = df_preprocess.dropna(subset=['fix'])
+    # timeline order
+    df_preprocess = df_preprocess[::-1]
+    df_preprocess = df_preprocess.reset_index(drop=True)
+    # contains_bug
+    df_preprocess['target'] = df_preprocess['target'].astype('int')
+    # fixes
+    df_preprocess['commit_hash_fix'] = df_preprocess['fixes'].dropna().apply(
+        lambda x: re.findall('\\b\\w+\\b', x)[0])
+    df_fix = df_preprocess[['commit_hash', 'timestamp']
+                           ].set_index('commit_hash')
+    return df_preprocess.join(df_fix, on='commit_hash_fix', how='left', rsuffix='_fix')
+
+
+def prequential(df_preprocess):
+    prequential_cols = ['timestamp', 'timestamp_fix'] + \
+        FEATURES + ['target']
+    df_prequential = df_preprocess[prequential_cols].copy()
+    df_prequential['timestep'] = range(len(df_prequential))
+    return df_prequential
--- jitsdp/evaluation.py
+++ jitsdp/evaluation.py
@@ -2,6 +2,7 @@ from jitsdp import metrics
 from jitsdp.classifier import Classifier
 from jitsdp.pipeline import Pipeline
 from jitsdp.plot import plot_recalls_gmean
+from jitsdp.data import make_stream, FEATURES
 
 import numpy as np
 import pandas as pd
@@ -15,7 +16,6 @@ from sklearn.preprocessing import StandardScaler
 import random
 from scipy.stats import mstats
 import math
-import re
 
 import logging
 
@@ -23,44 +23,12 @@ import logging
 logging.getLogger('').handlers = []
 logging.basicConfig(filename='logs/mlp.log', filemode='w', level=logging.DEBUG)
 
-
-df = pd.read_csv('https://raw.githubusercontent.com/dinaldoap/jit-sdp-data/master/brackets.csv')
-df.head()
-
-
-label_col = 'target'
-features_cols = ['fix', 'ns', 'nd', 'nf', 'entrophy', 'la', 'ld', 'lt', 'ndev', 'age', 'nuc', 'exp', 'rexp', 'sexp']
-preprocess_cols = ['commit_hash', 'author_date_unix_timestamp', 'fixes'] + features_cols + ['contains_bug']
-df_preprocess = df[preprocess_cols].copy()
-# timestamp
-df_preprocess = df_preprocess.rename(columns={'author_date_unix_timestamp': 'timestamp',
-                                                'contains_bug': label_col})
-# filter rows with missing data 
-df_preprocess = df_preprocess.dropna(subset=['fix'])
-# timeline order
-df_preprocess = df_preprocess[::-1]
-df_preprocess = df_preprocess.reset_index(drop=True)
-# contains_bug
-df_preprocess[label_col] = df_preprocess[label_col].astype('int')
-# fixes
-df_preprocess['commit_hash_fix'] = df_preprocess['fixes'].dropna().apply(lambda x: re.findall('\\b\\w+\\b', x)[0])
-df_fix = df_preprocess[['commit_hash', 'timestamp']].set_index('commit_hash')
-df_preprocess = df_preprocess.join(df_fix, on='commit_hash_fix', how='left', rsuffix='_fix')
-df_preprocess.head()
-
-
-
-prequential_cols = ['timestamp', 'timestamp_fix'] + features_cols + [label_col]
-df_prequential = df_preprocess[prequential_cols].copy()
-df_prequential['timestep'] = range(len(df_prequential))
-df_prequential.head()
-
-
+df_prequential = make_stream('https://raw.githubusercontent.com/dinaldoap/jit-sdp-data/master/brackets.csv')
 
 def create_pipeline():
     scaler = StandardScaler()
     criterion = nn.BCELoss()
-    classifier = Classifier(input_size=len(features_cols), hidden_size=len(features_cols) // 2, drop_prob=0.2)
+    classifier = Classifier(input_size=len(FEATURES), hidden_size=len(FEATURES) // 2, drop_prob=0.2)
     optimizer = optim.Adam(params=classifier.parameters(), lr=0.003)
     return Pipeline(steps=[scaler], classifier=classifier, optimizer=optimizer, criterion=criterion,
                     max_epochs=50, batch_size=512, fading_factor=1)
@@ -97,8 +65,8 @@ interval = 500 # commits
 end = len(df_prequential) # last commit
 n_chunks = math.ceil(end / interval)
 end = n_chunks * interval # last chunk end
-start = end - (n_chunks - 1) * interval # start test with second chunk
-#start = end - interval # use last two chunks to test
+#start = end - (n_chunks - 1) * interval # start test with second chunk
+start = end - interval # use last two chunks to test
 
 pipeline = create_pipeline()
 pipeline.save()
@@ -109,16 +77,16 @@ for current in range(start, end, interval):
     df_test = df_prequential[current:min(current + interval, end)].copy()
     # check if fix has been done (bug) or verification latency has passed (normal), otherwise exclude commit
     train_timestamp = df_train['timestamp'].max()
-    df_train[label_col] = df_train.apply(lambda row: 1 if row.timestamp_fix <= train_timestamp else (0 if row.timestamp <= train_timestamp - verification_latency else None), axis='columns')
-    df_unlabeled = df_train[pd.isnull(df_train[label_col])]
-    df_train = df_train.dropna(subset=[label_col])
-    df_train[label_col] = df_train[label_col].astype('int')
+    df_train['target'] = df_train.apply(lambda row: 1 if row.timestamp_fix <= train_timestamp else (0 if row.timestamp <= train_timestamp - verification_latency else None), axis='columns')
+    df_unlabeled = df_train[pd.isnull(df_train['target'])]
+    df_train = df_train.dropna(subset=['target'])
+    df_train['target'] = df_train['target'].astype('int')
     # convert to numpy array
-    X_train = df_train[features_cols].values
-    y_train = df_train[label_col].values
-    X_test = df_test[features_cols].values
-    y_test = df_test[label_col].values
-    X_unlabeled = df_unlabeled[features_cols].values
+    X_train = df_train[FEATURES].values
+    y_train = df_train['target'].values
+    X_test = df_test[FEATURES].values
+    y_test = df_test['target'].values
+    X_unlabeled = df_unlabeled[FEATURES].values
     y_unlabeled = np.zeros(len(X_unlabeled), dtype=np.int64)
     # train and evaluate
     pipeline = create_pipeline()
@@ -129,7 +97,7 @@ for current in range(start, end, interval):
     predictions.append(predictions_test)
 
 predictions = np.concatenate(predictions)
-results = df_prequential[['timestep', label_col]].copy()
+results = df_prequential[['timestep', 'target']].copy()
 results['prediction'] = predictions
 
 
