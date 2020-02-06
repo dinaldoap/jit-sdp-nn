3de45a832e0c2c3b41019a6619f454de3a19a9bb
dinaldoap <dinaldoap@gmail.com>
2020-02-06 13:00:14 +0000

    feat: save to file

--- jitsdp/plot.py
+++ jitsdp/plot.py
@@ -1,14 +1,15 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
 
 sns.set(rc={'figure.figsize': (12, 9)})
 
 
-def plot_recalls_gmean(data):
+def plot_recalls_gmean(data, dir='logs'):
     data = data.melt(id_vars='timestep',
                     value_vars=['r0', 'r1', 'gmean'],
                     var_name='metric',
                     value_name='value')
     sns.lineplot(x='timestep', y='value',
                  hue='metric', data=data)
-    plt.show()
+    plt.savefig(os.path.join(dir, 'recalls_gmean.png'))
