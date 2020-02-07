c191c5f0431cfb4c8cbb292f57d0d5687ad2b5f8
dinaldoap <dinaldoap@gmail.com>
2020-02-07 12:30:45 +0000

    feat: create dir when it doesn't exist

--- jitsdp/plot.py
+++ jitsdp/plot.py
@@ -1,15 +1,23 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-import os
+from pathlib import Path
 
 sns.set(rc={'figure.figsize': (12, 9)})
 
+DIR = Path('logs')
 
-def plot_recalls_gmean(data, dir='logs'):
+def create_dir(dir):
+    dir.mkdir(parents=True, exist_ok=True)
+
+def plot_recalls_gmean(data, dir=DIR):
+    avg_gmean = data['gmean'].mean()
     data = data.melt(id_vars='timestep',
                     value_vars=['r0', 'r1', 'gmean'],
                     var_name='metric',
                     value_name='value')
-    sns.lineplot(x='timestep', y='value',
+    ax = sns.lineplot(x='timestep', y='value',
                  hue='metric', data=data)
-    plt.savefig(os.path.join(dir, 'recalls_gmean.png'))
+    ax.axhline(avg_gmean, ls='--', label='avg(gmean)')
+    plt.legend()
+    create_dir(dir)
+    plt.savefig(dir / 'recalls_gmean.png')
