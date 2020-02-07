6c50e47d21c7f16e7c0c226e5922947820efb97f
dinaldoap <dinaldoap@gmail.com>
2020-02-07 12:29:54 +0000

    test: use pathlib instead of strings

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -2,14 +2,16 @@ from jitsdp import plot
 
 import numpy as np
 import pandas as pd
-import os
+from pathlib import Path
 
-def test_plot_recalls_gmean(tmp_path):
+DIR = Path('tests/logs')
+
+def test_plot_recalls_gmean():
     data = pd.DataFrame({
         'timestep': [0, 1, 2],
         'r0':    [0., 1., 2.],
         'r1':    [1., 0., 8.],
         'gmean': [0., 0., 4.],
     })
-    plot.plot_recalls_gmean(data, dir=tmp_path)
-    assert (tmp_path / 'recalls_gmean.png').exists()
+    plot.plot_recalls_gmean(data, dir=DIR)
+    assert (DIR / 'recalls_gmean.png').exists()
