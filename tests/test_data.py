9adc23d2da0aed5d4a906389469d6b0636744a78
dinaldoap <dinaldoap@gmail.com>
2020-02-07 13:10:44 +0000

    test: save and load data

---
+++ tests/test_data.py
@@ -0,0 +1,17 @@
+from jitsdp.data import save_data, load_data
+
+from test_plot import DIR
+
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+
+def test_save_load():
+    expected = pd.DataFrame({
+        'col': [1, 2, 3]
+    })
+    filename = DIR / 'data.pickle'
+    save_data(expected, filename)
+    saved_loaded = load_data(filename)
+    assert_frame_equal(expected, saved_loaded)
