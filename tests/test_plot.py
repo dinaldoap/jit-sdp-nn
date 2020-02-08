b4b13bb66f3cf49cc0c6408a3f5b1b289c82d4c1
dinaldoap <dinaldoap@gmail.com>
2020-02-08 17:22:14 +0000

    test: use config to pass dataset

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -7,11 +7,14 @@ import pandas as pd
 
 
 def test_plot_recalls_gmean():
+    config = {
+        'dataset': 'brackets'
+    }
     data = pd.DataFrame({
         'timestep': [0, 1, 2],
         'r0':    [0., 1., 2.],
         'r1':    [1., 0., 8.],
         'gmean': [0., 0., 4.],
     })
-    plot.plot_recalls_gmean(data, dir=DIR)
-    assert (DIR / 'recalls_gmean.png').exists()
+    plot.plot_recalls_gmean(data=data, config=config, dir=DIR)
+    assert (DIR / config['dataset'] / 'recalls_gmean.png').exists()
