3500001add8d9b2ecc6d4f52682155841715ea55
dinaldoap <dinaldoap@gmail.com>
2020-02-11 19:22:01 +0000

    feat: add method to plot proportions

--- jitsdp/plot.py
+++ jitsdp/plot.py
@@ -22,3 +22,22 @@ def plot_recalls_gmean(data, config, dir=DIR):
     mkdir(subdir)
     plt.savefig(subdir / 'recalls_gmean.png')
     plt.clf()
+
+
+def plot_proportions(data, config, dir=DIR):
+    avg_p0 = data['p0'].mean()
+    avg_p1 = data['p1'].mean()
+    data = data.melt(id_vars='timestep',
+                     value_vars=['p0', 'p1'],
+                     var_name='metric',
+                     value_name='value')
+    ax = sns.lineplot(x='timestep', y='value',
+                      hue='metric', data=data)
+    ax.axhline(avg_p0, ls='--', c='black', label='avg(p0)={:.2f}'.format(avg_p0))
+    ax.axhline(avg_p1, ls='-.', c='black', label='avg(p1)={:.2f}'.format(avg_p1))
+    ax.set_title(config['dataset'])
+    plt.legend()
+    subdir = dir / config['dataset']
+    mkdir(subdir)
+    plt.savefig(subdir / 'proportions.png')
+    plt.clf()
