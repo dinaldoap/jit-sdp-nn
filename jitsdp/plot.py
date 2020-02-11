608586119b090f230d4faa8410e4695b63638dc7
dinaldoap <dinaldoap@gmail.com>
2020-02-11 19:43:11 +0000

    refactor: reuse code for plotting metrics

--- jitsdp/plot.py
+++ jitsdp/plot.py
@@ -8,36 +8,31 @@ sns.set(rc={'figure.figsize': (12, 9)})
 
 
 def plot_recalls_gmean(data, config, dir=DIR):
-    avg_gmean = data['gmean'].mean()
-    data = data.melt(id_vars='timestep',
-                     value_vars=['r0', 'r1', 'gmean'],
-                     var_name='metric',
-                     value_name='value')
-    ax = sns.lineplot(x='timestep', y='value',
-                      hue='metric', data=data)
-    ax.axhline(avg_gmean, ls='--', label='avg(gmean)={:.2f}'.format(avg_gmean))
-    ax.set_title(config['dataset'])
-    plt.legend()
-    subdir = dir / config['dataset']
-    mkdir(subdir)
-    plt.savefig(subdir / 'recalls_gmean.png')
-    plt.clf()
+    __plot_metrics(data=data, config=config, dir=dir, metrics=[
+                   'r0', 'r1', 'gmean'], filename='recalls_gmean.png')
 
 
 def plot_proportions(data, config, dir=DIR):
-    avg_p0 = data['p0'].mean()
-    avg_p1 = data['p1'].mean()
+    __plot_metrics(data=data, config=config, dir=dir, metrics=[
+                   'p0', 'p1'], filename='proportions.png')
+
+
+def __plot_metrics(data, config, dir, metrics, filename):
+    assert len(metrics) <= 3, 'Only support three or less metrics.'
+    avgs = [data[metric].mean() for metric in metrics]
     data = data.melt(id_vars='timestep',
-                     value_vars=['p0', 'p1'],
+                     value_vars=metrics,
                      var_name='metric',
                      value_name='value')
     ax = sns.lineplot(x='timestep', y='value',
                       hue='metric', data=data)
-    ax.axhline(avg_p0, ls='--', c='black', label='avg(p0)={:.2f}'.format(avg_p0))
-    ax.axhline(avg_p1, ls='-.', c='black', label='avg(p1)={:.2f}'.format(avg_p1))
+    styles = ['--', '-.', '-']
+    for i, metric in enumerate(metrics):
+        ax.axhline(avgs[i], ls=styles[i], c='black',
+                   label='avg({})={:.2f}'.format(metric, avgs[i]))
     ax.set_title(config['dataset'])
     plt.legend()
     subdir = dir / config['dataset']
     mkdir(subdir)
-    plt.savefig(subdir / 'proportions.png')
+    plt.savefig(subdir / filename)
     plt.clf()
