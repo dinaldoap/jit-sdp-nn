369fbbc73552227bbdfe59ed6e187a00575f06f1
dinaldoap <dinaldoap@gmail.com>
2020-02-12 13:36:27 +0000

    feat: add method to split argument

--- jitsdp/utils.py
+++ jitsdp/utils.py
@@ -1,2 +1,8 @@
 def mkdir(dir):
     dir.mkdir(parents=True, exist_ok=True)
+
+
+def split_arg(argv, name):
+    value_index = argv.index(name) + 1
+    value = argv[value_index]
+    return argv[:value_index] + value.split() + argv[value_index + 1:]
