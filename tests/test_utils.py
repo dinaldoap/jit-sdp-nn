59b0c93a3dd2ae11d59ceff0f198da20361b6eb7
dinaldoap <dinaldoap@gmail.com>
2020-02-12 13:47:31 +0000

    test: check when argument is not in list

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,3 +8,10 @@ def test_split_arg():
     expected = ['exec', '--arg_to_split', 'value1',
                 'value2', '--arg_ok', 'value1', 'value2']
     assert expected == splitted
+
+
+def test_split_arg_not_in_list():
+    args = ['exec', '--arg_ok', 'value1', 'value2']
+    splitted = split_arg(args, '--arg_to_split')
+    expected = ['exec', '--arg_ok', 'value1', 'value2']
+    assert expected == splitted
