710f9fce12bc9a6c848dad799214bf1040a1645c
dinaldoap <dinaldoap@gmail.com>
2020-02-12 12:28:49 +0000

    refactor: format

--- jitsdp/__main__.py
+++ jitsdp/__main__.py
@@ -4,20 +4,28 @@ import logging
 import argparse
 import mlflow
 
+
 def args_to_config(args):
     config = dict(vars(args))
     del config['datasets']
     return config
 
+
 def main():
     parser = argparse.ArgumentParser(
         description='JIT-SDP: experiment execution')
-    parser.add_argument('command',   type=str, help='Which command should execute (default: run).',    default='run', choices=['run', 'report'])
-    parser.add_argument('--epochs',   type=int, help='Number of epochs performed by the training (default: 1).',    default=1)
-    parser.add_argument('--start',   type=int, help='First commit to be used for testing (default: 1000). A minimum of one fold is not used despite this parameter.',    default=1000)
-    parser.add_argument('--folds',   type=float, help='Fraction of folds to be used by the evaluation. A minimum of two folds is always used despite this parameter. (default: 0).',  default=0)
-    parser.add_argument('--fold_size',   type=int, help='Number of commits in each fold (default: 50).',    default=50)
-    parser.add_argument('--datasets',   type=str, help='Datasets to run the experiment. (default: brackets).', default=['brackets'], choices=['brackets', 'fabric8', 'neutron'], nargs='+')
+    parser.add_argument('command',   type=str, help='Which command should execute (default: run).',
+                        default='run', choices=['run', 'report'])
+    parser.add_argument('--epochs',   type=int,
+                        help='Number of epochs performed by the training (default: 1).',    default=1)
+    parser.add_argument('--start',   type=int,
+                        help='First commit to be used for testing (default: 1000). A minimum of one fold is not used despite this parameter.',    default=1000)
+    parser.add_argument('--folds',   type=float,
+                        help='Fraction of folds to be used by the evaluation. A minimum of two folds is always used despite this parameter. (default: 0).',  default=0)
+    parser.add_argument('--fold_size',   type=int,
+                        help='Number of commits in each fold (default: 50).',    default=50)
+    parser.add_argument('--datasets',   type=str, help='Datasets to run the experiment. (default: brackets).',
+                        default=['brackets'], choices=['brackets', 'fabric8', 'neutron'], nargs='+')
     args = parser.parse_args()
     print('Configuration: {}'.format(args))
     logging.getLogger('').handlers = []
