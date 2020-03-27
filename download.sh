d7101e3fabe3fe38ed49d45632b66dd200382c56
dinaldoap <dinaldoap@gmail.com>
2020-03-27 01:00:30 +0000

    feat: adjust artifact paths
    The local directory structure is different from the server.

--- download.sh
+++ download.sh
@@ -13,6 +13,8 @@ rm -rf ${store}
 mkdir ${store}
 # decompress
 tar -C ${store} -zxvf mlruns.tar.gz
+# change paths
+find mlruns-server -name meta.yaml | xargs sed --in-place --expression "s/file:\/\/\/.*\/mlruns\//file:\/\/\/workspace\/${store}\//g"
 # clean up
 rm mlruns.tar.gz
 ssh gpu rm ${project}/mlruns.tar.gz
