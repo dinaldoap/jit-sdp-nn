5cf61fc0d1c2664a9b9aea2e0a1d0539417a1cf2
dinaldoap <dinaldoap@gmail.com>
2020-04-15 20:03:26 +0000

    feat: add report with tables

---
+++ jitsdp/report.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df_runs = pd.read_csv('data/runs.csv')
+df_runs = df_runs.dropna(subset=['params.dataset'])
+df_groups = df_runs.groupby(by=['params.dataset', 'params.model']).agg({
+    'metrics.avg_gmean': ['mean', 'std']})
+print(df_groups)
+
+df_tops = df_groups.reset_index()
+df_tops = df_tops.sort_values(
+    by=[('metrics.avg_gmean', 'mean')], ascending=False)
+df_tops = df_tops.groupby(by=['params.dataset']).agg(
+    {'params.model': ['first']})
+print(df_tops)
