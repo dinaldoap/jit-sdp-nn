8c84c5343ccfff6c0d09890cda8d6e21e139cacd
dinaldoap <dinaldoap@gmail.com>
2020-04-20 13:30:03 +0000

    feat: add difference between recalls

--- jitsdp/report.py
+++ jitsdp/report.py
@@ -3,7 +3,7 @@ import pandas as pd
 df_runs = pd.read_csv('data/runs.csv')
 df_runs = df_runs.dropna(subset=['params.dataset'])
 df_groups = df_runs.groupby(by=['params.dataset', 'params.model']).agg({
-    'metrics.avg_gmean': ['mean', 'std']})
+    'metrics.avg_gmean': ['mean', 'std'], 'metrics.avg_r0-r1': ['mean', 'std']})
 print(df_groups)
 
 df_tops = df_groups.reset_index()
