762e91d715a3261b9fa04b1d36c858cdd50a16b3
dinaldoap <dinaldoap@gmail.com>
2020-08-02 14:27:18 +0000

    fix: change recursion limit
    This is necessary to pyinstaller creates the bundle.

--- release.sh
+++ release.sh
@@ -7,3 +7,10 @@ pyinstaller __main__.py  --onefile --name jitsdp \
                                     --hidden-import='sklearn.neighbors._typedefs' \
                                     --hidden-import='sklearn.neighbors._quad_tree' \
                                     --hidden-import='sklearn.tree._utils'
+
+echo '# -*- mode: python ; coding: utf-8 -*-' >> jitsdp_fixed.spec
+echo 'import sys' >> jitsdp_fixed.spec
+echo 'sys.setrecursionlimit(10000)' >> jitsdp_fixed.spec
+cat jitsdp.spec >> jitsdp_fixed.spec
+
+pyinstaller jitsdp_fixed.spec
