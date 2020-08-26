56cec3d566083cf1b7efb3011e883ec092ca558a
dinaldoap <dinaldoap@gmail.com>
2020-08-26 12:40:00 +0000

    feat: add ma window

--- jitsdp/orb.py
+++ jitsdp/orb.py
@@ -1,5 +1,17 @@
+import numpy as np
+from skmultiflow.trees import HoeffdingTreeClassifier
 from skmultiflow.meta import OzaBaggingClassifier
 
 
 class ORB(OzaBaggingClassifier):
-    pass
+
+    def __init__(self):
+        super().__init__(base_estimator=HoeffdingTreeClassifier(), n_estimators=20)
+        self.ma_window_size = 100
+        self.ma_window = np.array([.5] * self.ma_window_size)
+
+    def predict(self, X):
+        predictions = super().predict(X)
+        size = min(self.ma_window_size, len(predictions))
+        self.ma_window[-size:] = predictions[-size:]
+        return predictions
