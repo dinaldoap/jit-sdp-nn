d4e6298f34b32d4e5e8d4b918af6e87d321db0dd
dinaldoap <dinaldoap@gmail.com>
2020-09-04 02:16:46 +0000

    feat: commands with experiment, seed and dataset

--- jitsdp/grid.py
+++ jitsdp/grid.py
@@ -1,58 +1,119 @@
 # coding=utf-8
-from itertools import product
+import itertools
+
+
+class Experiment():
+
+    def __init__(self, experiment_config, seed_dataset_configs, models_configs):
+        self.experiment_config = self.fix_experimnt_config(experiment_config)
+        self.seed_dataset_configs = seed_dataset_configs
+        self.models_configs = models_configs
+
+    def fix_experimnt_config(self, experiment_config):
+        self.rate_driven = experiment_config['rate_driven']
+        self.meta_model = experiment_config['meta_model']
+        new_experiment_config = dict(experiment_config)
+        del new_experiment_config['rate_driven']
+        del new_experiment_config['meta_model']
+        return new_experiment_config
+
+    @property
+    def name(self):
+        rate_driven = 'r' if self.rate_driven else ''
+        model = self.experiment_config['model']
+        train_data = 'cp' if self.experiment_config['cross_project'] else 'wp'
+        return '{}{}-{}-{}'.format(rate_driven, self.meta_model, model, train_data)
+
+    def to_shell(self):
+        with open('jitsdp/dist/{}.sh'.format(self.name), mode='w') as out:
+            for seed_dataset_config in self.seed_dataset_configs:
+                for models_config in self.models_configs:
+                    config = dict()
+                    config.update(self.experiment_config)
+                    config.update(seed_dataset_config)
+                    config.update(models_config)
+                    config = self.fix_rate_driven(config)
+                    config, entrypoint = self.fix_meta_model(config)
+                    config = self.add_experiment_name(config)
+                    params = ['--{} {}'.format(key, value)
+                              for key, value in config.items()]
+                    params = ' '.join(params)
+                    out.write(
+                        './{} {}\n'.format(entrypoint, params))
+
+    def fix_rate_driven(self, config):
+        config = dict(config)
+        config['{}-rd'.format(self.meta_model)] = self.rate_driven
+        return config
+
+    def fix_meta_model(self, config):
+        config = dict(config)
+        meta_model_to_entrypoint = {
+            'orb': 'baseline',
+            'borb': 'jitsdp',
+        }
+        return config, meta_model_to_entrypoint[self.meta_model]
+
+    def add_experiment_name(self, config):
+        config = dict(config)
+        config['experiment-name'] = self.name
+        return config
 
 
 def main():
-    general = {
+    # experiments
+    orb_rorb_grid = {
+        'meta_model': ['orb'],
+        'cross_project': [0, 1],
+        'rate_driven': [0, 1],
+        'model': ['hts'],
+    }
+    borb_rborb_grid = {
+        'meta_model': ['borb'],
+        'cross_project': [0, 1],
+        'rate_driven': [0, 1],
+        'model': ['ihf'],
+    }
+    rborb_grid = {
+        'meta_model': ['borb'],
+        'cross_project': [0, 1],
+        'rate_driven': [1],
+        'model': ['lr', 'mlp', 'nb', 'irf'],
+    }
+    experiment_configs = [
+        orb_rorb_grid,
+        borb_rborb_grid,
+        rborb_grid,
+    ]
+    # seeds and datasets
+    experiment_configs = map(grid_to_configs, experiment_configs)
+    experiment_configs = itertools.chain.from_iterable(experiment_configs)
+    seed_dataset_configs = {
         'seeds': [0, 1, 2, 3, 4],
         'datasets': ['brackets', 'camel', 'fabric8', 'jgroups', 'neutron', 'tomcat', 'broadleaf', 'nova', 'npm', 'spring-integration'],
-        'cross_project': [0, 1]
     }
-    lr = {
-        'models': ['lr'],
-        'lr_alpha': [.001, .01, .1],
-        'lr_l1_ratio': [.15, .5, .85],
-        'lr_n_epochs': [10, 30, 50],
+    seed_dataset_configs = grid_to_configs(seed_dataset_configs)
+    # meta-models and models
+    models_configs = {
+        'hts': [{'hts-n-estimators': 1}],
+        'ihf': [],
+        'lr': [],
+        'mlp': [],
+        'nb': [],
+        'irf': [],
     }
-    lr.update(general)
-    mlp = {
-        'models': ['mlp'],
-        'mlp_n_hidden_layers': [1, 2, 3, 4],
-        'mlp_learning_rate': [.0001, .001, .01, .1, .3],
-        'mlp_n_epochs': [10, 30, 50],
-    }
-    mlp.update(general)
-    nb = {
-        'models': ['nb'],
-        'nb_n_updates': [10, 30, 50],
-    }
-    nb.update(general)
-    irf = {
-        'models': ['irf'],
-        'irf_n_estimators': [50, 100, 150],
-        'irf_criterion': ['entropy', 'gini'],
-        'irf_max_depth': [3, 5, 7],
-        'irf_max_features': [3, 5, 7],
-        'irf_min_samples_leaf': [50, 100, 150],
-        'irf_min_impurity_decrease': [.01, .02, .03]
-    }
-    irf.update(general)
-    grids = [
-        lr,
-        mlp,
-        nb,
-        irf,
-    ]
-    with open('jitsdp/dist/grid.sh', mode='w') as out:
-        for grid in grids:
-            keys = grid.keys()
-            values_lists = grid.values()
-            for values_tuple in product(*values_lists):
-                params = ['--{} {}'.format(key, values_tuple[i])
-                          for i, key in enumerate(keys)]
-                params = ' '.join(params)
-                out.write(
-                    './jitsdp run {}\n'.format(params))
+    for experiment_config in experiment_configs:
+        model = experiment_config['model']
+        experiment = Experiment(experiment_config=experiment_config,
+                                seed_dataset_configs=seed_dataset_configs, models_configs=models_configs[model])
+        experiment.to_shell()
+
+
+def grid_to_configs(grid):
+    keys = grid.keys()
+    values_lists = grid.values()
+    values_tuples = itertools.product(*values_lists)
+    return map(lambda values_tuple: dict(zip(keys, list(values_tuple))), values_tuples)
 
 
 if __name__ == '__main__':
