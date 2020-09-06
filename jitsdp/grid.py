453a95f02331ded0cf571c91a77051b8c64c3e03
dinaldoap <dinaldoap@gmail.com>
2020-09-06 19:19:17 +0000

    feat: change parameter type

--- jitsdp/grid.py
+++ jitsdp/grid.py
@@ -215,8 +215,8 @@ def meta_model_shared_config_space():
             uniform('{}-waiting-time'.format(meta_model), 90, 180, 30),
             uniform('{}-ma-window-size'.format(meta_model), 50, 200, 50),
             uniform('{}-th'.format(meta_model), .3, .5, .05),
-            loguniform('{}-l0'.format(meta_model), 1, 20),
-            loguniform('{}-l1'.format(meta_model), 1, 20),
+            loguniform('{}-l0'.format(meta_model), 1., 20.),
+            loguniform('{}-l1'.format(meta_model), 1., 20.),
             uniform('{}-m'.format(meta_model), 1.1, np.e, .2),
         ]
     return config_spaces
