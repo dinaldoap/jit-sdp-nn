7fc96b12c1df1429aad11a6dd2ff3c09b3cbcdc2
dinaldoap <dinaldoap@gmail.com>
2020-09-07 13:01:45 +0000

    feat: rename columns

--- jitsdp/test.py
+++ jitsdp/test.py
@@ -9,10 +9,12 @@ def best_configs():
     print(df_tuning.head(1))
     config_cols = config_columns(df_tuning.columns)
     df_best_configs = df_tuning.groupby(by=config_cols, as_index=False).agg({
-        'metrics.avg_gmean': 'mean', 'tags.run.command': 'first'})
+        'metrics.avg_gmean': ['mean', 'std'], 'tags.run.command': 'first'})
     df_best_configs.columns = remove_columns_prefix(df_best_configs.columns)
-    df_best_configs.sort_values(by='avg_gmean', ascending=False)
-    df_best_configs.drop_duplicates(subset=['meta_model', 'model', 'dataset'])
+    df_best_configs = df_best_configs.sort_values(
+        by='avg_gmean.mean', ascending=False)
+    df_best_configs = df_best_configs.drop_duplicates(
+        subset=['meta_model', 'model', 'dataset'])
     print(len(df_best_configs))
     print(df_best_configs.columns)
     print(df_best_configs.head(1))
@@ -23,7 +25,17 @@ def config_columns(cols):
 
 
 def remove_columns_prefix(cols):
-    return ['.'.join(col.split('.')[1:]) for col in cols]
+    new_cols = []
+    for col in cols:
+        first_level = col[0]
+        first_level = first_level.split('.')
+        first_level = '.'.join(first_level[1:])
+        second_level = col[1]
+        second_level = second_level if len(
+            second_level) == 0 else '.{}'.format(second_level)
+        new_col = '{}{}'.format(first_level, second_level)
+        new_cols.append(new_col)
+    return new_cols
 
 
 if __name__ == '__main__':
