e16fc5a12357f2b2ba5907de36069c10ed1e7fed
dinaldoap <dinaldoap@gmail.com>
2020-09-21 11:53:18 +0000

    feat: search borb-sample-size with uniform distribution

--- jitsdp/tuning.py
+++ jitsdp/tuning.py
@@ -153,7 +153,7 @@ def create_models_configs(config):
     borb = {}
     borb.update(meta_model_shared['borb'])
     borb.update([uniform('borb-pull-request-size', 50, 200, 50),
-                 loguniform('borb-sample-size', 1000, 8000, 1000),
+                 uniform('borb-sample-size', 1000, 4000, 1000),
                  ])
 
     ihf = {}
