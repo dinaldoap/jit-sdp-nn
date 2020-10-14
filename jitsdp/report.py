91ac586624d504bff08c13e7f75539456e828f5b
dinaldoap <dinaldoap@gmail.com>
2020-10-14 13:29:01 +0000

    feat: add wilcoxon test

--- jitsdp/report.py
+++ jitsdp/report.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 import numpy as np
 import pandas as pd
 import mlflow
-from scipy.stats import friedmanchisquare
+from scipy.stats import friedmanchisquare, wilcoxon
 import sys
 
 
@@ -114,12 +114,15 @@ def statistical_analysis(config, df_testing, metrics):
                                              'cross_project'], as_index=False).agg({'name': 'first', metric.column: 'mean'})
         df_inferential = pd.pivot_table(
             df_inferential, columns='name', values=metric.column, index='dataset')
+        _, wilcoxon_p_value = wilcoxon(
+            df_inferential['BORB-IHF'], df_inferential['ORB-OHT'], alternative='less' if metric.ascending else 'greater')
         measurements = [df_inferential[column]
                         for column in df_inferential.columns]
-        test_stat, p_value = friedmanchisquare(*measurements)
+        _, friedman_p_value = friedmanchisquare(*measurements)
         dir = dir_to_path(config['filename'])
         with open(dir / '{}.txt'.format(metric.column), 'w') as f:
-            f.write('p-value: {}'.format(p_value))
+            f.write('Friedman p-value: {}\n'.format(friedman_p_value))
+            f.write('Wilcoxon p-value: {}'.format(wilcoxon_p_value))
 
         avg_rank = df_inferential.rank(
             axis='columns', ascending=metric.ascending)
